c605ad8caf401e09d9eff473bc285245bf414d6a
Mironov Boris <lboris201@yandex.ru>
2026-04-18 15:04:56 +0300

    Initialized game service structure

---
+++ backend/services/game-service/internal/errors/errors.go
@@ -0,0 +1,13 @@
+package errors
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var ErrInternal = status.Error(codes.Internal, "internal error")
+
+var ErrUserWithSuchEmailExists = status.Error(codes.AlreadyExists, "email taken")
+var ErrUserWithSuchNicknameExists = status.Error(codes.AlreadyExists, "nickname taken")
+var ErrUserNotFound = status.Error(codes.NotFound, "user is not found")
+var ErrUserUnauthenticated = status.Error(codes.Unauthenticated, "user is unauthenticated")
