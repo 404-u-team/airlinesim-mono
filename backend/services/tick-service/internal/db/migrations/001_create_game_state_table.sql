cdd71db5a2c862b08d69dbcc80b4abed4cb6114f
Mironov Boris <lboris201@yandex.ru>
2026-05-12 18:26:16 +0300

    Small fixes

--- backend/services/tick-service/internal/db/migrations/001_create_game_state_table.sql
+++ backend/services/tick-service/internal/db/migrations/001_create_game_state_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE game_state (
-    id INTEGER PRIMARY KEY.
+    id INTEGER PRIMARY KEY,
     last_processed_15_min TIMESTAMP,
     last_processed_1_hour TIMESTAMP
 );
--- backend/services/tick-service/internal/tick/tick_loop.go
+++ backend/services/tick-service/internal/tick/tick_loop.go
@@ -9,6 +9,7 @@ import (
 	"github.com/404-u-team/airlinesim-mono/backend/tick-service/internal/kafka"
 	"github.com/404-u-team/airlinesim-mono/backend/tick-service/internal/repository"
 	"github.com/404-u-team/airlinesim-mono/backend/tick-service/internal/utils"
+	"github.com/jackc/pgx/v5"
 )
 
 type TickLoop interface {
@@ -31,12 +32,16 @@ func NewTickLoop(kafkaProducer kafka.Producer, gameStateRepo repository.GameStat
 func (tl *tickLoop) Run(ctx context.Context) error {
 	// get initial state of game in db
 	lastProccessed15Min, lastProccessed1Hour, err := tl.gameStateRepo.GetState(ctx)
-	if err != nil {
+	if err != nil && err != pgx.ErrNoRows {
 		return fmt.Errorf("got error when tried to get state from repository, %w", err)
 	}
-
-	tl.lastProccessed15Min = lastProccessed15Min
-	tl.lastProccessed1Hour = lastProccessed1Hour
+	if err == pgx.ErrNoRows {
+		tl.lastProccessed15Min = time.Unix(tl.config.StartGameTime, 0)
+		tl.lastProccessed1Hour = time.Unix(tl.config.StartGameTime, 0)
+	} else {
+		tl.lastProccessed15Min = lastProccessed15Min
+		tl.lastProccessed1Hour = lastProccessed1Hour
+	}
 
 	for {
 		currentGameTime := utils.CurrentGameTime(tl.config)
@@ -60,14 +65,14 @@ func (tl *tickLoop) Run(ctx context.Context) error {
 
 		// check is enough time elapsed (1 hour)
 		if currentGameTime.Sub(tl.lastProccessed1Hour) > time.Hour {
-			// send event - elapsed 15 min
+			// send event - elapsed 1 hour
 			err := tl.kafkaProducer.Send(ctx, kafka.Topic1Hour, nil, nil)
 			if err != nil {
 				return fmt.Errorf("got error when tried to send event using kafka, %w", err)
 			}
 
 			// put new processed time inside struct and db
-			newLastProcessed1Hour := tl.lastProccessed15Min.Add(time.Hour)
+			newLastProcessed1Hour := tl.lastProccessed1Hour.Add(time.Hour)
 			tl.lastProccessed1Hour = newLastProcessed1Hour
 			err = tl.gameStateRepo.SetLastProcessed1Hour(ctx, newLastProcessed1Hour)
 			if err != nil {
